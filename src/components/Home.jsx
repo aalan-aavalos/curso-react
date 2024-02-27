47ee3ae9eac05ce12db950e430d3316735264788
aalan <avalosalan789@gmail.com>
2024-02-27 09:54:37 -0600

    oapeaogfjagpuhgreapihoijgrsoijgre

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import NavigationBar from "./NavigationBar";
+
+function Home() {
+  return (
+    <div>
+      <NavigationBar />
+      <h1>Hola</h1>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div>
+            <h1>Pagina de perfil</h1>
+        </div>
+    );
+}
+
+export default Profile;
